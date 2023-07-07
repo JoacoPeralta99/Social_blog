f89f3d74b0bedf6633f5610a82a81964330658ec
joaco peralta <joacoperalta99@outlook.com>
2023-07-07 16:31:58 -0600

    first commit

---
+++ social_django/social/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+from django.db.models.signals import post_save
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='batman.png')
+
+    def __str__(self):
+        return f'perfil de {self.user.username}'
+    
+class Post(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE , related_name='posts')
+    timestamp = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content}'
+
+
+    
+
---
+++ social_django/social/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from .forms import UserRegisterForm , PostForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+def feed(request):
+    posts = Post.objects.all()
+
+
+
+    context = {'posts': posts}
+    return render(request, 'social/feed.html',context)
+
+
+def register(request):              # registro
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request , f'Usuario {username} creado')
+            return redirect('feed')
+    else:
+        form = UserRegisterForm()
+    
+    context = {'form' : form }
+    return render (request,'social/register.html', context)
+
+
+def post(request):
+    current_user = get_object_or_404(User, pk=request.user.pk)
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user=current_user
+            post.save()
+            messages.success(request, 'Post enviado')
+            return redirect('feed')
+    else:
+        form = PostForm()
+    return render(request, 'social/post.html', {'form': form})
+
+
+
+
+def profile(request, username= None):
+    current_user = request.user
+    if username and username != current_user.username:
+        user = User.objects.get(username=username)
+        posts = user.posts.all()
+
+    else:
+        posts = current_user.posts.all()
+        user = current_user
+    return render (request, 'social/profile.html',{'user':user, 'posts':post})
+
