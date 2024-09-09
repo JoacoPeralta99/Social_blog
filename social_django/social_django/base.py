b4ae0e22075047620cf8e8f223edb79730d4f2de
joaco peralta <joacoperalta99@outlook.com>
2024-09-09 01:13:59 -0600

    fixed errors ui & login

--- social_django/social_django/base.py
+++ social_django/social_django/base.py
@@ -144,7 +144,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-LOGIN_REDIRECT_URL = 'feed'
+LOGIN_REDIRECT_URL = 'blog:feed'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
